d2a8a8af41d283b5453c00f1538cc05fc872152f
Cheng Zhang <showt2me@gmail.com>
2021-03-22 00:20:24 +0900

    Download file cache with file type surrfix.

--- Example/CZHttpFileDemo/CZHttpFileDemo/ContentView.swift
+++ Example/CZHttpFileDemo/CZHttpFileDemo/ContentView.swift
@@ -7,22 +7,29 @@ import CZAVPlayer
  file:///Users/administrator/Library/Developer/CoreSimulator/Devices/8FF21713-4F10-4410-9700-AFE7376AECCE/data/Containers/Bundle/Application/BD017B4B-CAB3-4486-9ECC-1C233241C5D1/CZHttpFileDemo.app/a25fb258deab6f447a400683739521e6.m4a
  
  Cache:
+ file:///Users/administrator/Library/Developer/CoreSimulator/Devices/8FF21713-4F10-4410-9700-AFE7376AECCE/data/Containers/Data/Application/97180F65-D623-47E3-B55F-0694511282B1/Documents/CZHttpFileCache/a25fb258deab6f447a400683739521e6.m4a
+ 
+ *Cache from CZHttpFileManager:
+ file:///Users/administrator/Library/Developer/CoreSimulator/Devices/8FF21713-4F10-4410-9700-AFE7376AECCE/data/Containers/Data/Application/098E3EE4-5CC3-44CF-AEDF-BFE48BDEB65C/Documents/CZHttpFileCache/a25fb258deab6f447a400683739521e6
  */
 struct ContentView: View {
-  let url = URL(string: "https://github.com/geekaurora/terrace/raw/master/media/starter01.m4a")!
+  //let url = URL(string: "https://github.com/geekaurora/terrace/raw/master/media/starter01.m4a")!
+  let url = URL(string: "https://d37t5b145o82az.cloudfront.net/pictures/01bff78eae0870a01ed491ef86405bdf.jpg")!
 
   var cacheFileUrl: URL? {
     if true {
+      // * Note: should use `getCacheFileInfo` because sessionPath changes for each run.
+      // * Needs to surfix with .m4a
       let cacheFileInfo = CZHttpFileManager.shared.cache.getCacheFileInfo(forURL: url)
       return cacheFileInfo.fileURL
     } else {
-       return Bundle.main.url(forResource: "a25fb258deab6f447a400683739521e6", withExtension: "m4a")
+       return Bundle.main.url(forResource: "a25fb258deab6f447a400683739521e6", withExtension: nil)
     }
   }
 
   var body: some View {
-    let audioInfo = CZAudioInfo(url: cacheFileUrl, title: "Test")
-    AVPlayerView(audioInfo: audioInfo)
+//    let audioInfo = CZAudioInfo(url: cacheFileUrl, title: "Test")
+//    AVPlayerView(audioInfo: audioInfo)
     
     Button("Download") {
       CZHttpFileManager.shared.downloadFile(
