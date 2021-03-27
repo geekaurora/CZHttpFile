61828546c44878635cfe8425b92b2a3305666ad6
Cheng Zhang <showt2me@gmail.com>
2021-03-27 18:39:52 +0900

    Minor update.

--- Example/CZHttpFileDemo/CZHttpFileDemo/UI Layer/FeedCell.swift
+++ Example/CZHttpFileDemo/CZHttpFileDemo/UI Layer/FeedCell.swift
@@ -28,7 +28,7 @@ struct FeedCell: View {
     }
     .onAppear {
       let cachedFileURL =  CZHttpFileManager.shared.cachedFileURL(forURL: feed.url)
-      dbgPrint("cachedFileURL = \(cachedFileURL)")
+      // dbgPrint("cachedFileURL = \(cachedFileURL)")
       CZHttpFileManager.shared.downloadFile(
         url: feed.url,
         progress: { (currSize: Int64, totalSize: Int64, downloadURL: URL) in
