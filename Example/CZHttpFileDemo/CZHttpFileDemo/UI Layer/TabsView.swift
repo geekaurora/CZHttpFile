0d8f8f698fae03e91a08f5efa14d2be2763eb178
Cheng Zhang <showt2me@gmail.com>
2021-03-28 18:34:46 +0900

    SegmentControl: DownloadedList works.

--- Example/CZHttpFileDemo/CZHttpFileDemo/UI Layer/TabsView.swift
+++ Example/CZHttpFileDemo/CZHttpFileDemo/UI Layer/TabsView.swift
@@ -3,43 +3,67 @@ import CZUtils
 import CZHttpFile
 import CZAVPlayer
 
+/*
+ struct TabsView: View {
+     @State private var favoriteColor = 0
+
+     var body: some View {
+         VStack {
+             Picker(selection: $favoriteColor, label: Text("What is your favorite color?")) {
+                 Text("Red").tag(0)
+                 Text("Green").tag(1)
+                 Text("Blue").tag(2)
+             }
+             .pickerStyle(SegmentedPickerStyle())
+
+             Text("Value: \(favoriteColor)")
+         }
+     }
+ }
+
+ */
 struct TabsView: View {
-  
+  @State private var favoriteColor = 0
+
   var body: some View {
-    // SingleDownloadView()
-    
-//    MultiDownloadsView()
-//      .onAppear() {
-//        CZHttpFileDownloaderConstant.shouldObserveOperations = true
-//        // CZHttpFileManager.Config.maxConcurrencies = 7
-//      }
-    
-    TabView {
-      SingleDownloadView()
-        .tabItem {
-          Image(systemName: "house")
-          Text("Home")
+    VStack {
+        Picker(selection: $favoriteColor, label: Text("What is your favorite color?")) {
+            Text("Red").tag(0)
+            Text("Green").tag(1)
+            Text("Blue").tag(2)
         }
-      
-      MultiDownloadsView()
-        .tabItem {
-          Image(systemName: "square.and.arrow.down")
-          Text("Download")
-        }
-      
-      CZDownloadingList()
-        .tabItem {
-          Image(systemName: "square.and.arrow.down")
-          Text("Downloading")
-        }
-      
+        .pickerStyle(SegmentedPickerStyle())
+
+      //CZDownloadingList()
       CZDownloadedList()
-        .tabItem {
-          Image(systemName: "square.and.arrow.down")
-          Text("Downloaded")
-        }
-            
     }
+    
+//    TabView {
+//      SingleDownloadView()
+//        .tabItem {
+//          Image(systemName: "house")
+//          Text("Home")
+//        }
+//
+//      MultiDownloadsView()
+//        .tabItem {
+//          Image(systemName: "square.and.arrow.down")
+//          Text("Download")
+//        }
+//
+//      CZDownloadingList()
+//        .tabItem {
+//          Image(systemName: "square.and.arrow.down")
+//          Text("Downloading")
+//        }
+//
+//      CZDownloadedList()
+//        .tabItem {
+//          Image(systemName: "square.and.arrow.down")
+//          Text("Downloaded")
+//        }
+//
+//    }
     
   }
 }
