796d8d01874d853e9c28c88eff42fbadfa110ff1
Cheng Zhang <showt2me@gmail.com>
2021-07-14 21:41:08 +0900

    add test

---
+++ Tests/CZHttpFileTests/CZHttpFileCacheTests/CZImageCacheTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+import CZUtils
+import CZTestUtils
+import CZNetworking
+@testable import CZHttpFile
+
+final class CZImageCacheTests: XCTestCase {
+  var imageCache: CZImageCache!
+  
+  override func setUp() {
+    imageCache = CZImageCache()
+  }
+  
+  func testDataTransformerToImage() {
+    guard #available(iOS 13.0, *) else {
+      assertionFailure("Should run tests above iOS 13.0.")
+      return
+    }
+    guard let image = UIImage(systemName: "heart.fill").assertIfNil else {
+      return
+    }
+    let imageData = image.pngData()!
+    let transformedImage = imageCache.transformMetadataToCachedData(imageData)
+    let transformedImageData = transformedImage!.pngData()!
+    XCTAssert(imageData == transformedImageData)
+  }
+}
