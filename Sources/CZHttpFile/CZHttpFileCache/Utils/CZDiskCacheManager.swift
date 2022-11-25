76c7bacab82de3183525ceeec1eb3afc7c40d70a
Cheng Zhang <showt2me@gmail.com>
2022-11-25 11:18:57 -0800

    [Performance] Remove code - update last visited date.

--- Sources/CZHttpFile/CZHttpFileCache/Utils/CZDiskCacheManager.swift
+++ Sources/CZHttpFile/CZHttpFileCache/Utils/CZDiskCacheManager.swift
@@ -162,8 +162,6 @@ extension CZDiskCacheManager {
       
       if let data = try? Data(contentsOf: fileURL),
          let image = self.transformMetadataToCachedData(data).assertIfNil {
-        // Update last visited date
-        // self.setCachedItemsDict(key: cacheKey, subkey: CacheConstant.kFileVisitedDate, value: NSDate(), skipIfKeyNotExists: true)
         completion(image)
       } else {
         completion(nil)
