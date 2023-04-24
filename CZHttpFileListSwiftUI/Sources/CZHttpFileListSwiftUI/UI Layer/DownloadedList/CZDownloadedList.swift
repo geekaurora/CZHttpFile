d3b97ce629b7255b1b9d426714460ccc704f0a1c
Cheng Zhang <showt2me@gmail.com>
2023-04-23 17:21:55 -0700

    minor

--- CZHttpFileListSwiftUI/Sources/CZHttpFileListSwiftUI/UI Layer/DownloadedList/CZDownloadedList.swift
+++ CZHttpFileListSwiftUI/Sources/CZHttpFileListSwiftUI/UI Layer/DownloadedList/CZDownloadedList.swift
@@ -5,15 +5,9 @@ import CZHttpFile
 struct CZDownloadedList: View {
   @ObservedObject
   var listState = CZDownloadedListState()
-  @State
-  var currentCacheSize = 0
-  
-  func refresh() {
-    self.currentCacheSize = CZHttpFileManager.shared.cache.currentCacheSize
-  }
   
   var body: some View {
-    let cacheInfo = "currentCacheSize = \(currentCacheSize.sizeString) \nmaxCacheSize = \(CZHttpFileManager.shared.cache.maxCacheSize.sizeString)"
+    let cacheInfo = "currentCacheSize = \(listState.currentCacheSize.sizeString) \nmaxCacheSize = \(CZHttpFileManager.shared.cache.maxCacheSize.sizeString)"
     
     VStack {
       Text(cacheInfo)
@@ -21,7 +15,7 @@ struct CZDownloadedList: View {
       
       Button("Clear All Cache") {
         CZHttpFileManager.shared.cache.clearCache() {
-          self.refresh()
+          listState.refreshCurrentCacheSize()
           CZAlertManager.showAlert(message: "Cleared all cache!")
         }
       }
@@ -29,8 +23,9 @@ struct CZDownloadedList: View {
       List(listState.downloads, id: \.diffId) {
         CZDownloadingCell(download: $0)
       }
-    }.onAppear {
-      refresh()
     }
+//    .onAppear {
+//      listState.refreshCurrentCacheSize()
+//    }
   }
 }
